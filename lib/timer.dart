98807ca1f2929cb7ef865f880f3119d5dd237132
ahyaemon <iamahyaemon@gmail.com>
2019-11-03 11:13:11 +0900

    add start timer button with no implementation

--- lib/timer.dart
+++ lib/timer.dart
@@ -11,10 +11,31 @@ class TimerState extends State<Timer> {
 
   @override
   Widget build(BuildContext context) {
-    return Text(_formatDuration(remainingTime));
+    return Container(
+        padding: const EdgeInsets.all(50.0),
+        child: Column(
+          children: <Widget>[
+          Text(_formatDuration(remainingTime)),
+          FlatButton(
+            onPressed: _startTimer,
+            color: Colors.blue,
+            child: Text(
+              '開始',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          )
+        ],
+      )
+    );
   }
 
   String _formatDuration(Duration duration) {
     return duration.toString().split(".")[0];
+  }
+
+  void _startTimer() {
+
   }
 }

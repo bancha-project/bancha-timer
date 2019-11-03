7ee4ccad65f0efd703d0882e562fc70f4b9c779e
ahyaemon <iamahyaemon@gmail.com>
2019-11-03 10:26:00 +0900

    update title

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Bancha Timer',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Bancha Timer'),
     );
   }
 }
